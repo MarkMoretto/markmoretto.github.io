b78584b5a4b4d79b1f5991cbc2b91c95b3407dcb
Mark M <otteromkram@gmail.com>
2020-05-21 08:04:23 -0400

    testing preact

--- assets/js/site/my-lit-html.js
+++
@@ -1,8 +0,0 @@
-
-// import {html, render} from './node_modules/lit-html/lit-html.js';
-import {html, render} from 'lit-html';
-
-const myTemplate = (name) => html`<p>Hello ${name}</p>`;
-
-
-render(myTemplate('World'), document.body);
---
+++ assets/js/site/preact-test.js
@@ -0,0 +1,5 @@
+
+import { h, render, Component } from 'preact';
+
+// Target portion of document
+const document_main = document.body.children[1].children[1];
